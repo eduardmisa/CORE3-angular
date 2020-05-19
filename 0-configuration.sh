64e479b26c1773adb866a385c1449318e2f9da2e
tir.eduardo <eduardo@tirsolutions.com>
2020-05-19 20:47:11 +0800

    added deployment files

---
+++ 0-configuration.sh
@@ -0,0 +1,4 @@
+export DEPLOYMENT_PATH="/var/www/core3-angular.u4rdsystem.com"
+export GIT_LINK="https://eduardmisa@github.com/eduardmisa/CORE3-angular.git"
+export GIT_BRANCH="master"
+export GIT_PASSWORD="531ed96864Z"
