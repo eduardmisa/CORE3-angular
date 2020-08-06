d42660d3620043c1c26c7ff4a8badcc4e55e46d0
eduardmisa <eduardmisa@gmail.com>
2020-08-06 09:11:34 +0800

    new

--- 0-configuration.sh
+++
@@ -1,4 +0,0 @@
-export DEPLOYMENT_PATH="/var/www/netcore-angular.emisa.dev"
-export GIT_LINK="https://eduardmisa@github.com/eduardmisa/CORE3-angular.git"
-export GIT_BRANCH="master"
-export GIT_PASSWORD="531ed96864Z"
---
+++ deploy/manual/0-configuration.sh
@@ -0,0 +1,3 @@
+export DEPLOYMENT_PATH="/var/www/netcore-angular.emisa.dev"
+export GIT_LINK="git@github.com:eduardmisa/CORE3-angular.git"
+export GIT_BRANCH="master"
